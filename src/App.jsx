3929f2d42c9ac1ce1a10c8f1e7a15610afc2e4bd
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-05-17 11:13:41 +0200

    changed app.js to app.jsx and we have a gameboard 10x10

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import GameboardPage from './pages/GameboardPage';
+
+function App() {
+	return (
+    	<div id="App">
+			
+			<GameboardPage />
+			
+		</div>
+	);
+}
+
+export default App;
---
+++ src/pages/GameboardPage.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+
+const GameboardPage = () => {
+    return (
+        <>
+           <h1>Battleship</h1>
+            <div className="wrapper">
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+                <div className='row'>
+                    <div className="box">1</div>
+                    <div className="box">2</div>
+                    <div className="box">3</div>
+                    <div className="box">4</div>
+                    <div className="box">5</div>
+                    <div className="box">6</div>
+                    <div className="box">7</div>
+                    <div className="box">8</div>
+                    <div className="box">9</div>
+                    <div className="box">10</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default GameboardPage
